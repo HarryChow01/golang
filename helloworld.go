265ca0d4a6885f7446c5bbc954429b1b4b245185
harrychow01 <harrychow01@163.com>
2021-09-23 13:54:59 +0800

    添加文件

--- helloworld.go
+++ helloworld.go
@@ -3,7 +3,25 @@ package main
 
 import "fmt"
 
+func fun1() {
+    sum := 0
+    for i := 0; i <= 10; i++ {
+        sum += i
+    }
+    fmt.Println(sum)
+}
+
+func fun2() {
+    sum := 0
+    for i := 0; i <= 10; i++ {
+        sum += i
+    }
+    fmt.Println("sum:", sum)
+}
+
+
 func main() {
     fmt.Println("Hello, world")
+    fun2()
 }
 
