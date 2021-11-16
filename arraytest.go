d32c200f4ea93fe65e9a09a0a8b51d50153dc533
harrychow01 <harrychow01@163.com>
2021-11-16 20:25:10 +0800

    测试指针

--- arraytest.go
+++ arraytest.go
@@ -9,6 +9,22 @@ func ArrayTest() {
     fmt.Printf("len(arr): %+v", len(arr))
 }
 
+func testPointer1(intValue int32) {
+    fmt.Printf("func intValue: %v\n", intValue)
+    intValue = 15
+    fmt.Printf("func intValue: %v\n", intValue)
+}
+
+func testPointer2(intValue *int32) {
+    fmt.Printf("func intValue: %v\n", *intValue)
+    *intValue = 15
+    fmt.Printf("func intValue: %v\n", *intValue)
+}
+
 func main() {
-    ArrayTest()
+    // ArrayTest()
+    var intValue int32 = 10
+    fmt.Printf("main intValue: %v\n", intValue)
+    testPointer2(&intValue)
+    fmt.Printf("main intValue: %v\n", intValue)
 }
---
+++ redistest.go
@@ -0,0 +1,16 @@
+
+package main
+
+import (
+    "fmt"
+)
+
+func redisTest1() {
+
+}
+
+
+func main() {
+    fmt.Println("Hello, world")
+    redisTest1()
+}
