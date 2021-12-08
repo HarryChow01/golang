ec01a7192447fe97857d34e7f6e1ba734513d1f0
harrychow01 <harrychow01@163.com>
2021-12-08 19:17:57 +0800

    jsontest

--- jsontest.go
+++ jsontest.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os"
 )
 
 type Student struct {
@@ -34,7 +36,38 @@ func jsonTest1() {
 	fmt.Println("student2:", student2)
 }
 
+type HoloConfig struct {
+	HoloHost     string `json:"holo_host"`
+	HoloPort     int16  `json:"holo_port"`
+	HoloUser string `json:"holo_user"`
+	HoloPassword string `json:"holo_password"`
+	HoloDBName   string `json:"holo_dbname"`
+}
+
+func loadJsonFile(filePath string) []byte {
+	fmt.Printf("filePath: %s\n", filePath)
+	file, err := os.Open(filePath)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	content, err := ioutil.ReadAll(file)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("content: %s\n", string(content))
+	return content
+}
+
 
 func main() {
-	jsonTest1()
+	filePath := "conf/conf.json"
+	fileContent := loadJsonFile(filePath)
+
+	holoConfig := HoloConfig{}
+	err := json.Unmarshal(fileContent, &holoConfig)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("holoConfig: %+v\n", holoConfig)
 }
--- redistest.go
+++ redistest.go
@@ -3,7 +3,21 @@ package main
 
 import (
     "fmt"
+    "path"
+    "runtime"
 )
+
+func getCurrentAbPathByCaller() string {
+    var abPath string
+    _, filename, _, ok := runtime.Caller(0)
+    fmt.Printf("cur file: %s\n", filename)
+    if ok {
+        abPath = path.Dir(filename)
+    }
+    fmt.Printf("absolute path: %s\n", abPath)
+    return abPath
+}
+
 
 func redisTest1() {
 
@@ -11,6 +25,6 @@ func redisTest1() {
 
 
 func main() {
-    fmt.Println("Hello, world")
-    redisTest1()
+    fmt.Println("start:")
+    getCurrentAbPathByCaller()
 }
