af26ea71dde8412469d5c0de249540672304c75a
harrychow01 <harrychow01@163.com>
2022-02-03 22:46:53 +0800

    slice

--- leetcode/array_leet_test.go
+++ leetcode/array_leet_test.go
@@ -57,3 +57,27 @@ func merge(a, b []int) []int {
 	}
 	return res
 }
+
+func reverse(s []int) {
+	for i, j := 0, len(s) - 1; i < j; i, j = i + 1, j - 1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
+
+func TestReverse(t *testing.T) {
+	data := []int{1, 2, 3, 4, 5, 6, 7}
+	fmt.Println(data)
+	// reverse(data[:])
+	reverse(data)
+	fmt.Println(data)
+}
+
+func TestRotateLeftK(t *testing.T) {
+	data := []int{1, 2, 3, 4, 5, 6, 7}
+	fmt.Println(data)
+	reverse(data[:2])
+	reverse(data[2:])
+	reverse(data)
+	fmt.Println(data)
+}
+
--- test/array_test.go
+++ test/array_test.go
@@ -6,14 +6,33 @@ import (
 )
 
 func TestArray1(t *testing.T) {
-    var arr1 [12]int
+    var arr1 [4]int
     for index, value := range arr1 {
         fmt.Printf("index: %+v, value: %+v\n", index, value)
     }
-    arr2 := [12]int{10, 20, 30}
+    arr2 := [4]int{10, 20, 30}
     for index, value := range arr2 {
         fmt.Printf("index: %+v, value: %+v\n", index, value)
     }
+    arr3 := [...]int{1, 2}
+    for index, value := range arr3 {
+    fmt.Printf("index: %+v, value: %+v\n", index, value)
+    }
+}
+
+type Currency int
+
+const (
+    USD Currency = iota // 美元
+    EUR                 // 欧元
+    GBP                 // 英镑
+    RMB                 // 人民币
+)
+
+func TestArray2(t *testing.T) {
+    symbol := [...]string{USD: "$", EUR: "€", GBP: "￡", RMB: "￥"}
+
+    fmt.Println(RMB, symbol[RMB]) // "3 ￥"
 }
 
 func testPointer1(intValue int32) {
