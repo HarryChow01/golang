007063abd109aad1014ee9e55f76e8064cfafc32
harrychow01 <harrychow01@163.com>
2022-03-27 22:23:23 +0800

    main

--- main.go
+++ main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"fmt"
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.String(200, "Hello, world")
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+	fmt.Println("Hello, world")
 }
