5b21b3ac8042b09a4208eb31529f35eef62e9543
harrychow01 <harrychow01@163.com>
2022-04-22 17:15:03 +0800

    Default Changelist

--- concurrent/select1/main.go
+++ concurrent/select1/main.go
@@ -2,22 +2,20 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func server1(ch chan string) {
-	time.Sleep(1 * time.Second)
 	ch <- "from server1"
 }
 
 func server2(ch chan string) {
-	time.Sleep(3 * time.Second)
 	ch <- "from server2"
 }
 
 func main() {
 	ch1 := make(chan string)
 	ch2 := make(chan string)
+
 	go server1(ch1)
 	go server2(ch2)
 
