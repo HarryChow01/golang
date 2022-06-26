1842271ded8227a5b8f1beccb85766ba85b8afca
harrychow01 <harrychow01@163.com>
2022-06-26 11:22:45 +0800

    mapt

--- test/map_test.go
+++ test/map_test.go
@@ -5,6 +5,41 @@ import (
 	"testing"
 )
 
+var mGlobal1 map[string]int
+var mGlobal2 = map[string]int{
+	"aa": 11,
+	"bb": 22,
+}
+
+func TestMapGlobal1(t *testing.T) {
+	fmt.Printf("mGlobal1 == nil: %t\n", mGlobal1 == nil)
+	fmt.Printf("len(mGlobal1): %d\n", len(mGlobal1))
+
+	fmt.Printf("mGlobal2 == nil: %t\n", mGlobal2 == nil)
+	fmt.Printf("len(mGlobal2): %d\n", len(mGlobal2))
+}
+
+func TestMapLocal1(t *testing.T) {
+	var mLocal map[string]int
+	fmt.Printf("mLocal == nil: %t\n", mLocal == nil)
+	fmt.Printf("len(mLocal): %d\n", len(mLocal))
+
+	// ok
+	m1 := map[string]int{}
+	m2 := map[string]int{
+		"aa": 11,
+		"bb": 22,
+	}
+	fmt.Printf("m1 == nil: %t\n", m1 == nil)
+	fmt.Printf("m2 == nil: %t\n", m2 == nil)
+	fmt.Printf("len(m1): %d\n", len(m1))
+	fmt.Printf("len(m2): %d\n", len(m2))
+
+	m3 := make(map[string]int)
+	fmt.Printf("m3 == nil: %t\n", m3 == nil)
+	fmt.Printf("len(m3): %d\n", len(m3))
+}
+
 func TestMap1(t *testing.T) {
 	map1 := make(map[string]int)
 	map1["aa"] = 11
@@ -26,6 +61,17 @@ func TestMap1(t *testing.T) {
 
 	for key := range map2 {
 		fmt.Printf("%s\t%d\n", key, map2[key])
+	}
+
+	// 判断某个Key是否存在
+	if _, ok := map2["Tony"]; ok {
+		fmt.Println("Tony is exists")
+	}
+
+	if value, ok := map2["aa"]; ok {
+		fmt.Printf("value: %d\n", value)
+	} else {
+		fmt.Printf("value: %d\n", value)
 	}
 
 }
