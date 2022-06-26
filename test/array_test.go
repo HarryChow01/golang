fed398db2dfbdb889ed72415d908bb097c7a7eab
harrychow01 <harrychow01@163.com>
2022-06-26 15:39:02 +0800

    array_test.go

--- test/array_test.go
+++ test/array_test.go
@@ -1,56 +1,67 @@
 package test
 
 import (
-    "fmt"
-    "testing"
+	"fmt"
+	"testing"
 )
 
 func TestArray1(t *testing.T) {
-    var arr1 [4]int
-    for index, value := range arr1 {
-        fmt.Printf("index: %+v, value: %+v\n", index, value)
-    }
-    arr2 := [4]int{10, 20, 30}
-    for index, value := range arr2 {
-        fmt.Printf("index: %+v, value: %+v\n", index, value)
-    }
-    arr3 := [...]int{1, 2}
-    for index, value := range arr3 {
-    fmt.Printf("index: %+v, value: %+v\n", index, value)
-    }
+	// 声明数组，不提供显式初始化
+	var arr1 [5]int // 数组声明即定义，自动分配内存并初始化
+	// fmt.Printf("m1 == nil: %t\n", arr1 == nil), error: 不能和nil比较
+	fmt.Printf("len(arr1): %d\n", len(arr1)) // 数组声明即定义，自动分配内存并初始化
+	fmt.Println(arr1)
+
+	// 左侧 [5]int 可以省略
+	// var arr2 [5]int = [5]int{1, 2, 3, 4, 5} // ok
+	var arr2 = [5]int{1, 2, 3, 4, 5}
+	// arr3 := [5]int{1,2,3,4,5} // 同上
+	fmt.Printf("len(arr2): %d\n", len(arr2)) // 数组声明即定义，自动分配内存并初始化
+	fmt.Println(arr2)
+
+	arr4 := [...]int{1, 2, 3, 4, 5}
+	for index, value := range arr4 {
+		fmt.Printf("index = %+v, value = %+v\n", index, value)
+	}
+
+	// 数组部分赋值
+	arr5 := [4]int{10, 20, 30}
+	for index, value := range arr5 {
+		fmt.Printf("index = %+v, value = %+v\n", index, value)
+	}
 }
 
 type Currency int
 
 const (
-    USD Currency = iota // 美元
-    EUR                 // 欧元
-    GBP                 // 英镑
-    RMB                 // 人民币
+	USD Currency = iota // 美元
+	EUR                 // 欧元
+	GBP                 // 英镑
+	RMB                 // 人民币
 )
 
 func TestArray2(t *testing.T) {
-    symbol := [...]string{USD: "$", EUR: "€", GBP: "￡", RMB: "￥"}
+	symbol := [...]string{USD: "$", EUR: "€", GBP: "￡", RMB: "￥"}
 
-    fmt.Println(RMB, symbol[RMB]) // "3 ￥"
+	fmt.Println(RMB, symbol[RMB]) // "3 ￥"
 }
 
 func testPointer1(intValue int32) {
-    fmt.Printf("func intValue: %v\n", intValue)
-    intValue = 15
-    fmt.Printf("func intValue: %v\n", intValue)
+	fmt.Printf("func intValue: %v\n", intValue)
+	intValue = 15
+	fmt.Printf("func intValue: %v\n", intValue)
 }
 
 func testPointer2(intValue *int32) {
-    fmt.Printf("func intValue: %v\n", *intValue)
-    *intValue = 15
-    fmt.Printf("func intValue: %v\n", *intValue)
+	fmt.Printf("func intValue: %v\n", *intValue)
+	*intValue = 15
+	fmt.Printf("func intValue: %v\n", *intValue)
 }
 
 func Test222(t *testing.T) {
-    // ArrayTest()
-    var intValue int32 = 10
-    fmt.Printf("main intValue: %v\n", intValue)
-    testPointer2(&intValue)
-    fmt.Printf("main intValue: %v\n", intValue)
+	// ArrayTest()
+	var intValue int32 = 10
+	fmt.Printf("main intValue: %v\n", intValue)
+	testPointer2(&intValue)
+	fmt.Printf("main intValue: %v\n", intValue)
 }
